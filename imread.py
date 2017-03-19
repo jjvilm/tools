5255265b239883dd02ffda62e384b793e58de703
Juan <jj.villela@gmail.com>
2017-03-19 16:34:01 -0500

    imread now successfully loops when out of range or frames

--- imread.py
+++ imread.py
@@ -11,7 +11,7 @@ frame_speed = 0.30 # .3 normilizes time
 # global variable for max frames
 t_n_frames = 0
 # exit switch for empty directory
-exit = False
+exit_switch = False
 
 def get_dir_size(start_path):
     global t_n_frames
@@ -24,15 +24,16 @@ def get_dir_size(start_path):
             counter += 1
     t_n_frames = counter - 1
     print("Size of directory: {:.2f}MB\nFrames: {}\n".format((total_size/1024.0)/1024.0, t_n_frames))
+    raw_input()
     if total_size <= 0:
-        return 1
-    return 0
+        return True
+    return False
 
 #folder_name = raw_input('Folder name: ')
 folder = 'sec-imgs'
 # exit if directory is empty
 if get_dir_size(folder):
-    exit = True
+    exit_switch = True
 
 
 #creates above folder if it does not exist
@@ -73,6 +74,7 @@ def frame_slider(x):
     frame_selected = c_frame
     new_frame_from_slider = True
     switch = True
+
 def set_frame_speed(x):
     global frame_speed
     slider_speed = cv2.getTrackbarPos('Speed','Frames-Control')
@@ -90,7 +92,6 @@ def set_frame_speed(x):
             10: 1
             }
     frame_speed = speeds[slider_speed]
-
 
 def frame_by_frame(current_frame_n):
     global imgs_list, folder
@@ -134,11 +135,10 @@ def resize(frame):
     resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
     return resized
 
-
-
 def main():
-    global frame_selected, imgs_list, new_frame_from_slider, switch, frame_speed, exit
-    if exit:
+    global exit_switch
+    # does not run main if folder is empty size is <= 0
+    if exit_switch:
         return
 
     # window for slider
@@ -148,83 +148,98 @@ def main():
     # Speed slider
     #cv2.createTrackbar('Speed','Frames-Control',0,10, set_frame_speed)
 
-    while True:
-        if frame_selected < 0:
-            frame_selected = 1
-        #print('Frame_selected = {}'.format(frame_selected))
+    def main_loop():
+        global frame_selected, imgs_list, new_frame_from_slider, switch, frame_speed, exit_switch
 
-        for n_current_frame,i in enumerate(imgs_list[frame_selected:]):
-            # reverts back to original with same frame
-            if n_current_frame < frame_selected:
-                continue
-           # Outputs frame to terminal
-            #print("Curr: {} Sel: {}".format(n_current_frame, frame_selected))
-            #print("Frame:{} ".format(n_current_frame))
+        while True:
+            if frame_selected < 0:
+                frame_selected = 1
+            #print('Frame_selected = {}'.format(frame_selected))
 
-            # skips first empty file
-            if i == '':
-                continue
-            # if slider turns on switch then skip till new frame
-            if switch:
-                if n_current_frame == frame_selected:
-                    switch = False
-                else:
+            for n_current_frame,i in enumerate(imgs_list[frame_selected:]):
+                # reverts back to original with same frame
+                if n_current_frame < frame_selected:
                     continue
-            # Sometimes imshow crashes on this line while 
-            # reading image file
-            try:
-                frame = cv2.imread(i, -1)
-                cv2.imshow("Frames",frame)
-            except Exception as e:
-                print(e,'\nBAD Frame')
-                continue
+               # Outputs frame to terminal
+                #print("Curr: {} Sel: {}".format(n_current_frame, frame_selected))
+                #print("Frame:{} ".format(n_current_frame))
 
-            key = cv2.waitKey(33) & 0xFF
+                # skips first empty file
+                if i == '':
+                    continue
+                # if slider turns on switch then skip till new frame
+                if switch:
+                    if n_current_frame == frame_selected:
+                        switch = False
+                    else:
+                        continue
+                # Sometimes imshow crashes on this line while 
+                # reading image file
+                try:
+                    frame = cv2.imread(i, -1)
+                    cv2.imshow("Frames",frame)
+                except Exception as e:
+                    print(e,'\nBAD Frame')
+                    continue
 
-            if key == ord('q'):
-                exit(0)
-            if key == ord('p'):
-                cv2.waitKey(0)
-            # Increases speed
-            if key == ord('>'):
-                if frame_speed != 0 and frame_speed >= .005:
-                    frame_speed -= .005
-                print('Frame speed changed to {}'.format(frame_speed))
-            # Decrases frame play
-            if key == ord('<'):
-                frame_speed += .005
-                print('Frame speed changed to {}'.format(frame_speed))
-            # Normal Speed
-            if key == ord('/'):
-                frame_speed  = 0
-                print('Frame speed changed to {}'.format(frame_speed))
-            # Frame selection prompt
-            if key == ord('f'):
-                print("Max frame == {}".format(len(imgs_list)))
-                frame_selected = frame_selection()
-                break
-            # Frame by frame paused
-            if key == ord('.'):
-                #frame_selected = frame_by_frame(frame_selected)
-                # goes into a loop that pauses frames
-                print(frame_selected)
-                print("current",n_current_frame)
-                frame_selected = frame_by_frame(n_current_frame)
-                print(frame_selected, n_current_frame)
-            # skips by 100 frames
-            if key == ord('='):
-                frame_selected += 100 
-                break
-            # backwards by 100 frames
-            if key == ord('-'):
-                frame_selected -= 100 
-                break
+                key = cv2.waitKey(33) & 0xFF
 
-            #print("Frame speed: {}".format(frame_speed))
-            time.sleep(frame_speed)
-            if new_frame_from_slider:
-                #new_frame_from_slider = False
-                break
+                if key == ord('q'):
+                    exit_switch = 1
+                    exit(0)
+                    return
+                if key == ord('p'):
+                    cv2.waitKey(0)
+                # Increases speed
+                if key == ord('>'):
+                    if frame_speed != 0 and frame_speed >= .005:
+                        frame_speed -= .005
+                    print('Frame speed changed to {}'.format(frame_speed))
+                # Decrases frame play
+                if key == ord('<'):
+                    frame_speed += .005
+                    print('Frame speed changed to {}'.format(frame_speed))
+                # Normal Speed
+                if key == ord('/'):
+                    frame_speed  = 0
+                    print('Frame speed changed to {}'.format(frame_speed))
+                # Frame selection prompt
+                if key == ord('f'):
+                    print("Max frame == {}".format(len(imgs_list)))
+                    frame_selected = frame_selection()
+                    break
+                # Frame by frame paused
+                if key == ord('.'):
+                    #frame_selected = frame_by_frame(frame_selected)
+                    # goes into a loop that pauses frames
+                    frame_selected = frame_by_frame(n_current_frame)
+                # skips by 100 frames
+                if key == ord('='):
+                    if (frame_selected+n_current_frame+200) < t_n_frames:
+                        frame_selected += 100 
+                        break
+                # backwards by 100 frames
+                if key == ord('-'):
+                    frame_selected -= 100 
+                    break
 
+                #print("Frame speed: {}".format(frame_speed))
+                time.sleep(frame_speed)
+                if new_frame_from_slider:
+                    #new_frame_from_slider = False
+                    break
+            else:
+                # frames end here so restart loop
+                print('Looping...')
+                frame_selected = 0
+
+    while 1:
+        try:
+            main_loop()
+        
+        except Exception as e:
+            print('major exception',e)
+            continue
+    
 
 main()
