9132378c5afa819fc159bbc7228829eb75be551b
Juan <jj.villela@gmail.com>
2017-04-30 18:59:04 -0500

    Added script that sends emails using gmail accounts

---
+++ send_email.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python2
+
+def send_email(user, pwd, recipient, subject, body):
+    import smtplib
+
+    gmail_user = user
+    gmail_pwd = pwd
+    FROM = user
+    TO = recipient if type(recipient) is list else [recipient]
+    SUBJECT = subject
+    TEXT = body
+
+    # Prepare actual message
+    message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+    """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(gmail_user, gmail_pwd)
+        server.sendmail(FROM, TO, message)
+        server.close()
+        print 'successfully sent the mail'
+    except:
+        print "failed to send mail"
+
+user = ""
+pwd = ""
+recipient = ""
+subject = ""
+body = ""
+
+send_email(user, pwd, recipient, subject, body)
